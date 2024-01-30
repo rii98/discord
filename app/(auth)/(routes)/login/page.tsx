f93bf6f421e0eaf32b7d5ab0ee10497c3e95cb00
rii98 <136151503+rii98@users.noreply.github.com>
2024-01-30 10:34:41 +0545

    sample project structure init

---
+++ app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginPage = () => {
+  return <div>Welcome to the login page!</div>;
+};
+
+export default LoginPage;
---
+++ app/(auth)/(routes)/register/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Register = () => {
+  return <div>Welcome to Register Page!</div>;
+};
+
+export default Register;
